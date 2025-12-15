8d434649f4da10e577f9a2cfeec7f2b9e09bbfc2
Téo Calvo <teocalvo2@gmail.com>
2025-12-15 11:37:55 -0300

    feat: streamelements

--- utils/db.go
+++ utils/db.go
@@ -10,14 +10,6 @@ import (
 
 func OpenDBConnection(settings *config.Config) (*gorm.DB, error) {
 
-	// godotenv.Load(".env")
-	// HOST_DB := os.Getenv("HOST_DB")
-	// PORT_DB := os.Getenv("PORT_DB")
-	// USER_DB := os.Getenv("USER_DB")
-	// PASSWORD_DB := os.Getenv("PASSWORD_DB")
-	// dsn := "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
-	// dsn = fmt.Sprintf(dsn, USER_DB, PASSWORD_DB, HOST_DB, PORT_DB, "teomebot")
-
 	db, err := gorm.Open(mysql.Open(settings.DsnMysql), &gorm.Config{})
 	if err != nil {
 		return nil, err
